e6c7ef0267a43d8ac640a7f647700fe072aae616
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-21 22:37:49 -0500

    setting up program (caporal)

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 
-const caporal = require("caporal");
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
+const program = require("caporal");
 
-const start = debounce(() => {
-  console.log("STARTING USERS PROGRAM");
-}, 100);
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of a file to execute")
+  .action(args => {
+    const start = debounce(() => {
+      console.log("STARTING USERS PROGRAM");
+    }, 100);
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => {
-    console.log("file changed");
-  })
-  .on("unlink", () => {
-    console.log("file Unlinked");
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", () => {
+        console.log("file changed");
+      })
+      .on("unlink", () => {
+        console.log("file Unlinked");
+      });
   });
 
-// console.log("changed");
+program.parse(process.argv);
